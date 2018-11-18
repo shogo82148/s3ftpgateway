d75b5603264fc6c96e64b26bef21c437af5b118e
Ichinose Shogo <shogo82148@gmail.com>
2018-11-19 07:44:44 +0900

    add writeReply method

--- ftp/conn.go
+++ ftp/conn.go
@@ -3,7 +3,7 @@ package ftp
 import (
 	"bufio"
 	"context"
-	"io"
+	"fmt"
 	"log"
 	"net"
 	"strings"
@@ -12,34 +12,87 @@ import (
 type conn struct {
 	server *Server
 	rwc    net.Conn
+	ctrlw  *bufio.Writer
 }
 
 func (c *conn) serve(ctx context.Context) {
-	io.WriteString(c.rwc, "220 Service ready\r\n")
+	c.ctrlw = bufio.NewWriter(c.rwc)
 	s := bufio.NewScanner(c.rwc)
+
+	c.writeReply(220, "Service ready")
 	for s.Scan() {
 		text := s.Text()
 		log.Println(text)
-		var cmd, arg string
-		if idx := strings.Index(text, " "); idx >= 0 {
-			cmd = strings.ToUpper(text[:idx])
-			arg = strings.TrimSpace(text[idx:])
-		} else {
-			cmd = strings.ToUpper(text)
-		}
-		_ = arg
+		cmd, _ := c.parseLine(text)
 		switch cmd {
 		case "USER":
-			io.WriteString(c.rwc, "331 User name ok, need password\r\n")
+			c.writeReply(331, "User name ok, need password")
 		case "PASS":
-			io.WriteString(c.rwc, "230 User logged in\r\n")
+			c.writeReply(230, "User logged in")
 		case "PWD":
-			io.WriteString(c.rwc, "257 dummy response\r\n")
+			c.writeReply(257, "dummy response")
 		default:
-			io.WriteString(c.rwc, "500 Command not found\r\n")
+			c.writeReply(500, "Command not found")
 		}
 	}
 	if err := s.Err(); err != nil {
 		log.Println(err)
 	}
+}
+
+func (c *conn) parseLine(line string) (string, string) {
+	var cmd, arg string
+	if idx := strings.Index(line, " "); idx >= 0 {
+		cmd = strings.ToUpper(line[:idx])
+		arg = strings.TrimSpace(line[idx:])
+	} else {
+		cmd = strings.ToUpper(line)
+	}
+	return cmd, arg
+}
+
+func (c *conn) writeReply(code int, messages ...string) (int, error) {
+	if len(messages) == 0 {
+		n, err := fmt.Fprintf(c.ctrlw, "%03d \r\n", code)
+		if err != nil {
+			return n, err
+		}
+		if err := c.ctrlw.Flush(); err != nil {
+			return n, err
+		}
+		return n, nil
+	} else if len(messages) == 1 {
+		// single line reply
+		n, err := fmt.Fprintf(c.ctrlw, "%03d %s\r\n", code, messages[0])
+		if err != nil {
+			return n, err
+		}
+		if err := c.ctrlw.Flush(); err != nil {
+			return n, err
+		}
+		return n, nil
+	}
+
+	// multiple lines reply
+	m := 0
+	n, err := fmt.Fprintf(c.ctrlw, "%03d-%s\r\n", code, messages[0])
+	m += n
+	if err != nil {
+		return m, err
+	}
+
+	for _, msg := range messages[1 : len(messages)-1] {
+		n, err := fmt.Fprintf(c.ctrlw, " %s\r\n", msg)
+		m += n
+		if err != nil {
+			return m, err
+		}
+	}
+
+	n, err = fmt.Fprintf(c.ctrlw, "%03d %s\r\n", code, messages[len(messages)-1])
+	m += n
+	if err != nil {
+		return m, err
+	}
+	return m, nil
 }
