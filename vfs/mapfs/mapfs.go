88da13ad0e7d2e13ed2181caf93889251bfced4c
Ichinose Shogo <shogo82148@gmail.com>
2018-11-28 16:08:24 +0900

    implement ReadDir of mapfs

--- vfs/mapfs/mapfs.go
+++ vfs/mapfs/mapfs.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"io"
 	"os"
-	"path"
+	pathpkg "path"
+	"sort"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/shogo82148/s3ftpgateway/vfs"
 )
@@ -29,7 +31,7 @@ func (fs *mapFS) String() string {
 }
 
 func filename(p string) string {
-	return strings.TrimPrefix(path.Clean(p), "/")
+	return strings.TrimPrefix(pathpkg.Clean(p), "/")
 }
 
 // Open opens the file.
@@ -44,6 +46,14 @@ func (fs *mapFS) Open(ctx context.Context, name string) (vfs.ReadSeekCloser, err
 	return nopCloser{strings.NewReader(b)}, nil
 }
 
+func fileInfo(name, contents string) os.FileInfo {
+	return mapFI{name: pathpkg.Base(name), size: len(contents)}
+}
+
+func dirInfo(name string) os.FileInfo {
+	return mapFI{name: pathpkg.Base(name), dir: true}
+}
+
 // Lstat returns a FileInfo describing the named file.
 func (fs *mapFS) Lstat(ctx context.Context, path string) (os.FileInfo, error) {
 	return nil, nil
@@ -54,9 +64,68 @@ func (fs *mapFS) Stat(ctx context.Context, path string) (os.FileInfo, error) {
 	return fs.Lstat(ctx, path)
 }
 
+// slashdir returns path.Dir(p), but special-cases paths not beginning
+// with a slash to be in the root.
+func slashdir(p string) string {
+	d := pathpkg.Dir(p)
+	if d == "." {
+		return "/"
+	}
+	if strings.HasPrefix(p, "/") {
+		return d
+	}
+	return "/" + d
+}
+
 // ReadDir reads the contents of the directory.
 func (fs *mapFS) ReadDir(ctx context.Context, path string) ([]os.FileInfo, error) {
-	return nil, nil
+	path = pathpkg.Clean(path)
+	var ents []string
+	fim := make(map[string]os.FileInfo) // base -> fi
+
+	fs.mu.RLock()
+	defer fs.mu.RUnlock()
+
+	for fn, b := range fs.m {
+		dir := slashdir(fn)
+		isFile := true
+		var lastBase string
+		for {
+			if dir == path {
+				base := lastBase
+				if isFile {
+					base = pathpkg.Base(fn)
+				}
+				if fim[base] == nil {
+					var fi os.FileInfo
+					if isFile {
+						fi = fileInfo(fn, b)
+					} else {
+						fi = dirInfo(base)
+					}
+					ents = append(ents, base)
+					fim[base] = fi
+				}
+			}
+			if dir == "/" {
+				break
+			} else {
+				isFile = false
+				lastBase = pathpkg.Base(dir)
+				dir = pathpkg.Dir(dir)
+			}
+		}
+	}
+	if len(ents) == 0 {
+		return nil, os.ErrNotExist
+	}
+
+	sort.Strings(ents)
+	var list []os.FileInfo
+	for _, dir := range ents {
+		list = append(list, fim[dir])
+	}
+	return list, nil
 }
 
 // Create creates the named file, truncating it if it already exists.
@@ -74,6 +143,25 @@ func (fs *mapFS) Mkdir(ctx context.Context, name string) error {
 func (fs *mapFS) Remove(ctx context.Context, name string) error {
 	return nil
 }
+
+// mapFI is the map-based implementation of FileInfo.
+type mapFI struct {
+	name string
+	size int
+	dir  bool
+}
+
+func (fi mapFI) IsDir() bool        { return fi.dir }
+func (fi mapFI) ModTime() time.Time { return time.Time{} }
+func (fi mapFI) Mode() os.FileMode {
+	if fi.IsDir() {
+		return 0755 | os.ModeDir
+	}
+	return 0444
+}
+func (fi mapFI) Name() string     { return pathpkg.Base(fi.name) }
+func (fi mapFI) Size() int64      { return int64(fi.size) }
+func (fi mapFI) Sys() interface{} { return nil }
 
 type nopCloser struct {
 	io.ReadSeeker
--- vfs/mapfs/mapfs_test.go
+++ vfs/mapfs/mapfs_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -47,6 +48,67 @@ func TestOpen(t *testing.T) {
 	}
 
 	_, err := fs.Open(ctx, "/xxxx")
+	if !os.IsNotExist(err) {
+		t.Errorf("ReadDir /xxxx = %v; want os.IsNotExist error", err)
+	}
+}
+
+func TestReaddir(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	fs := New(map[string]string{
+		"foo/bar/three.txt": "333",
+		"foo/bar.txt":       "22",
+		"top.txt":           "top.txt file",
+		"other-top.txt":     "other-top.txt file",
+	})
+	tests := []struct {
+		dir  string
+		want []os.FileInfo
+	}{
+		{
+			dir: "/",
+			want: []os.FileInfo{
+				mapFI{name: "foo", dir: true},
+				mapFI{name: "other-top.txt", size: len("other-top.txt file")},
+				mapFI{name: "top.txt", size: len("top.txt file")},
+			},
+		},
+		{
+			dir: "/foo",
+			want: []os.FileInfo{
+				mapFI{name: "bar", dir: true},
+				mapFI{name: "bar.txt", size: 2},
+			},
+		},
+		{
+			dir: "/foo/",
+			want: []os.FileInfo{
+				mapFI{name: "bar", dir: true},
+				mapFI{name: "bar.txt", size: 2},
+			},
+		},
+		{
+			dir: "/foo/bar",
+			want: []os.FileInfo{
+				mapFI{name: "three.txt", size: 3},
+			},
+		},
+	}
+	for _, tt := range tests {
+		fis, err := fs.ReadDir(ctx, tt.dir)
+		if err != nil {
+			t.Errorf("ReadDir(%q) = %v", tt.dir, err)
+			continue
+		}
+		if !reflect.DeepEqual(fis, tt.want) {
+			t.Errorf("ReadDir(%q) = %#v; want %#v", tt.dir, fis, tt.want)
+			continue
+		}
+	}
+
+	_, err := fs.ReadDir(ctx, "/xxxx")
 	if !os.IsNotExist(err) {
 		t.Errorf("ReadDir /xxxx = %v; want os.IsNotExist error", err)
 	}
