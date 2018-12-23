74339414f3e08e2a849ec5a250086d22c268e309
Ichinose Shogo <shogo82148@gmail.com>
2018-12-23 16:29:47 +0900

    add test for STOR command.

--- ftp/command_test.go
+++ ftp/command_test.go
@@ -145,6 +145,55 @@ done_testing;
 	perl.Prove(ctx, t, script, u.Host)
 }
 
+func TestStor(t *testing.T) {
+	perl, err := newPerlExecutor()
+	if err != nil {
+		t.Skipf("perl is required for this test: %v", err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fs := mapfs.New(map[string]string{})
+	ts := ftptest.NewServer(fs)
+	defer ts.Close()
+	ts.Config.Logger = testLogger{t}
+
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	script := `use utf8;
+use strict;
+use warnings;
+use Test::More;
+use Net::FTP;
+
+my $host = shift;
+my $ftp = Net::FTP->new($host, Debug => 1) or die "fail to connect ftp server: $@";
+ok $ftp->login('anonymous', 'foobar@example.com'), 'login';
+
+my $content = "Hello ftp!";
+open my $fh, "<", \$content;
+ok $ftp->put($fh, 'testfile'), 'put';
+ok $ftp->quit(), 'quit';
+done_testing;
+`
+
+	perl.Prove(ctx, t, script, u.Host)
+
+	r, err := fs.Open(ctx, "testfile")
+	if err != nil {
+		t.Error(err)
+	}
+	b, err := ioutil.ReadAll(r)
+	if string(b) != "Hello ftp!" {
+		t.Errorf("want Hello ftp!, got %s", b)
+	}
+}
+
 type testLogger struct {
 	t *testing.T
 }
