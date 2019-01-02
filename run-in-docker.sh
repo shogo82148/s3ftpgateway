3e68db506079ff45323fd0c219dfc823c6ccd1e8
Ichinose Shogo <shogo82148@gmail.com>
2019-01-02 18:10:59 +0900

    fix typo

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -2,6 +2,6 @@
 
 CURRENT=$(cd "$(dirname "$0")" && pwd)
 docker run --rm -it \
-    -e GO111MODULES=on \
+    -e GO111MODULE=on \
     -v "$CURRENT":/go/src/github.com/shogo82148/s3ftpgateway \
     -w /go/src/github.com/shogo82148/s3ftpgateway golang:1.11.4 "$@"
