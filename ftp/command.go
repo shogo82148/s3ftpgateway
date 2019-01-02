b6a8dc6a3de453f42afbf129146dd55f932e6f5f
Ichinose Shogo <shogo82148@gmail.com>
2019-01-02 21:43:17 +0900

    sleep if the user is not anonymous.

--- ftp/command.go
+++ ftp/command.go
@@ -501,7 +501,7 @@ func (commandPass) Execute(ctx context.Context, c *ServerConn, cmd *Command) {
 	auth, err := c.server.authorizer().Authorize(ctx, c, c.user, cmd.Arg)
 	if err != nil {
 		c.failCnt++
-		if c.failCnt > 1 {
+		if c.failCnt > 1 || !isAnonymous(c.user) {
 			if err := sleepWithContext(ctx, 5*time.Second); err != nil {
 				c.server.logger().Printf(c.sessionID, "fail to execute PASS command: %v", err)
 				c.closing = true
@@ -521,6 +521,10 @@ func (commandPass) Execute(ctx context.Context, c *ServerConn, cmd *Command) {
 	c.auth = auth
 	c.pwd = "/"
 	c.WriteReply(StatusLoggedIn, "User logged in, proceed.")
+}
+
+func isAnonymous(user string) bool {
+	return user == "anonymous" || user == "ftp"
 }
 
 func sleepWithContext(ctx context.Context, d time.Duration) error {
