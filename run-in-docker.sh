bbab64f28e6caae2af625b0d72598c6be176e22b
Ichinose Shogo <shogo82148@gmail.com>
2021-04-04 20:20:04 +0900

    bump Go v1.16.3

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -4,4 +4,4 @@ CURRENT=$(cd "$(dirname "$0")" && pwd)
 docker run --rm -it \
     -e GO111MODULE=on \
     -v "$CURRENT":/go/src/github.com/shogo82148/s3ftpgateway \
-    -w /go/src/github.com/shogo82148/s3ftpgateway golang:1.15.6 "$@"
+    -w /go/src/github.com/shogo82148/s3ftpgateway golang:1.16.3 "$@"
